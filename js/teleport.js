521aad3daef2b6202a97e77a86b6f02b3c865867
tanyuan <tanyuan@users.noreply.github.com>
2017-08-30 21:26:08 +0800

    Place tiles for teleportation

---
+++ js/teleport.js
@@ -0,0 +1,11 @@
+// Component to change to random color on click.
+AFRAME.registerComponent('cursor-listener', {
+  init: function () {
+    this.el.addEventListener('mouseenter', function (evt) {
+      this.setAttribute('material', 'color', '#555555');
+    });
+    this.el.addEventListener('mouseleave', function (evt) {
+      this.setAttribute('material', 'color', '#444444');
+    });
+  }
+});

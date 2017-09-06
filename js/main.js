958333cd180696cc1d88e42f9197b5cfa0d1339d
Steff Yang <wonderwallfrank@gmail.com>
2017-09-06 20:59:34 +0800

    stage one complete

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 // Global Variable Declaration
 var finite_state = 'wake';
-var in_event = false;
 var camera_plane = document.querySelector('a-camera');
 var event_plane = document.querySelector('#event-plane');
 var event_text = document.querySelector('#event-text');
 var action_text = document.querySelector('#action-text');
+var camera_text = document.querySelector('#camera-text');
+var help_text = document.querySelector('#help-text');
+var tile_array = document.getElementsByClassName('tile');
 
-console.log(camera_plane,event_plane,event_text);
 
 function set_event_plane() {
 	var current_position = camera_plane.getAttribute('position');
@@ -18,6 +19,57 @@ function set_event_plane() {
 function reset_event_plane() {
 	event_plane.setAttribute('position',"0 0 -20");
 	event_plane.setAttribute('rotation',"0 0 0");
+}
+
+// init first stage
+for (var i = tile_array.length - 1; i >= 0; i--) {
+	tile_array[i].setAttribute('visible',false);
+}
+
+setTimeout(function(){
+	camera_text.setAttribute('visible', true);
+	help_text.setAttribute('visible', true);
+},3000);
+
+function second_stage(){
+	camera_text.setAttribute('visible', true);
+	camera_text.setAttribute('text', {
+		width: 1, 
+		color: 'white', 
+		align: 'center', 
+		value: 'Where is this place? Did I got too drunk and sleep over at someone’s place yesterday?'
+	});
+	setTimeout(function(){
+		camera_text.setAttribute('text', {
+			width: 1, 
+			color: 'white', 
+			align: 'center', 
+			value: "Why? I can't memorise anything happened yesterday?"
+		});
+	},5000);
+	setTimeout(function(){
+		camera_text.setAttribute('text', {
+			width: 1, 
+			color: 'white', 
+			align: 'center', 
+			value: 'I need to find out where this place is!'
+		});
+		help_text.setAttribute('visible', true);
+		help_text.setAttribute('text', {
+			width: 1, 
+			color: 'black', 
+			align: 'center', 
+			value: 'Gaze at the floor tile to move, and explore the room.'
+		});
+	},10000);
+	setTimeout(function(){
+		camera_text.setAttribute('visible', false);
+		help_text.setAttribute('visible', false);
+		finite_state = 'free';
+		for (var i = tile_array.length - 1; i >= 0; i--) {
+			tile_array[i].setAttribute('visible',true);
+		}
+	},15000);
 }
 
 
--- js/pickup.js
+++ js/pickup.js
@@ -2,16 +2,19 @@ AFRAME.registerComponent('water-cup', {
 	init: function () {
 		var water = this.el;
 		water.addEventListener('click', function (evt) {
-			in_event = true;
 			// when water picked up
 			// 'wake stage'
 			if (finite_state === 'wake') {
+				finite_state = 'water';
 				set_event_plane();
 				water.setAttribute('visible', false);
+				camera_text.setAttribute('visible', false);
+				help_text.setAttribute('visible', false);
 
 				action_text.addEventListener('click', function(){
 					water.setAttribute('visible', true);
 					reset_event_plane();
+					second_stage();
 				});
 			} else {
 				// other stage of game

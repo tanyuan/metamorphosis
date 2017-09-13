7199498d23d8d5eb407860fad2cddba28be593ae
Steff Yang <wonderwallfrank@gmail.com>
2017-09-13 15:56:32 +0800

    compressed file

--- js/teleport.js
+++ js/teleport.js
@@ -1,18 +1,1 @@
-AFRAME.registerComponent('teleport-listener', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-      if (finite_state === 'free') {
-      	this.setAttribute('material', 'color', '#DDDDDD');
-      } else {
-      	return;
-      }
-    });
-    this.el.addEventListener('mouseleave', function (evt) {
-    	if (finite_state === 'free') {
-	      	this.setAttribute('material', 'color', '#444444');
-	      } else {
-	      	return;
-	      }
-    });
-  }
-});
+AFRAME.registerComponent("teleport-listener",{init:function(){this.el.addEventListener("mouseenter",function(e){"free"===finite_state&&this.setAttribute("material","color","#DDDDDD")}),this.el.addEventListener("mouseleave",function(e){"free"===finite_state&&this.setAttribute("material","color","#444444")})}});
